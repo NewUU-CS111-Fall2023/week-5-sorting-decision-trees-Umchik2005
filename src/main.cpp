dd77599d2b50f54ebf0bb7136251632489e8ac9e
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2023-11-16 17:56:13 +0500

    Creating file task_1.h

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,35 @@
  */
 
 #include <iostream>
+#include <string>
 #include "task_1.h"
+#include "task_2.h"
 
 int main() {
     std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    std::string booksTitle[] = {"Catcher in the Rye", 
+                                "Pride and Prejudice",
+                                "To Kill a Mockingbird",
+                                "The Great Gatsby", 
+                                "Moby Dick" 
+                                };
+    int size = sizeof(booksTitle) / sizeof(booksTitle[0]);
+
+    
+    bubbleSort(booksTitle, size);
+    printTitles(booksTitle, size);
+    std::cout << "////////////////////////////////////////////" << std::endl;
+    
+    
+    // std::cout << "Task 1" << std::endl;
+    // // call for task 1
+    // std::cout << "Task 2" << std::endl;
+    // // call for task 2
+    // std::cout << "Task 3" << std::endl;
+    // // call for task 3
+    // std::cout << "Task 4" << std::endl;
+    // // call for task 4
+    // std::cout << "Task 5" << std::endl;
+    // // call for task 5
     return 0;
 }
--- src/task_1.h
+++ src/task_1.h
@@ -3,3 +3,20 @@
  * Date:
  * Name:
  */
+void bubbleSort(std::string arr[], int size){
+    int i, j;
+        for (i = 0; i < size - 1; i++){
+            for(int j = 0; j < size - i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    std::swap(arr[j], arr[j+1]);
+                }
+            }
+        }
+    }  
+void printTitles(std::string bookTitles[], int size){
+    for(int k = 0; k < size; k++){
+        std::cout << bookTitles[k] << "\n";
+    }
+}  
+
+
